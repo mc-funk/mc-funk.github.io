9fd1a48f5e6761e97773d2f26facb5011b08eddf
Michelle C. Funk <michelle@michellefunk.com>
2015-06-25 11:03:26 -0500

    Add uswap project

--- PrimeWk3.js
+++ PrimeWk3.js
@@ -1,8 +1,8 @@
 //This function will take in one argument and return a number, to be called in a console.log command. 
 
 function CoolSocks(input) {
-	var length = input.length;
+	var length = prompt.length;
 	return (length/3.14159) * 10 + 42;
 }
-var socksPrompt = prompt("Enter a phrase to find out its mystery number.");
-console.log("The mystery number associated with \"" + socksPrompt + "\" is " + parseInt(CoolSocks(socksPrompt)) + ".");
+var prompt = prompt("Enter a phrase to find out its mystery number.");
+console.log("The mystery number associated with \"" + prompt + "\" is " + parseInt(CoolSocks(prompt)) + ".");
